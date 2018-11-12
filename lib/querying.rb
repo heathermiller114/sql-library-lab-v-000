d2d1c89bfee8e51e49191b083c354d2e6e096907
Heather Miller <heather.miller114@gmail.com>
2018-11-12 19:01:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) AS num_characters.species
   FROM characters
-  ORDER BY COUNT(*) DESC
+  ORDER BY num_characters.species DESC
   LIMIT 1";
 end
 
