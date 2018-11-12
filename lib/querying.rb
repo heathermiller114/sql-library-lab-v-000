9964c952db8a33f0663c22b4c4f59774dbf677df
Heather Miller <heather.miller114@gmail.com>
2018-11-12 18:42:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,8 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON characters.series_id = series.id
-  GROUP BY character"
+  GROUP BY characters
+  HAVING "
 end
 
 def select_character_names_and_number_of_books_they_are_in
