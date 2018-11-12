fd5b406b14dd6e3cc1a7e5fbc6a73a0f81e9a1c3
Heather Miller <heather.miller114@gmail.com>
2018-11-12 18:46:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,9 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON characters.series_id = series.id
   GROUP BY characters
-  HAVING COUNT(*)"
+  HAVING COUNT(*)
+  WHERE characters.species = human DESC
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
