71b968ff42c1721f9e1543b7fba0e63605748ab9
Heather Miller <heather.miller114@gmail.com>
2018-11-12 19:06:56 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,5 +43,5 @@ def select_character_names_and_number_of_books_they_are_in
   AS num_books
   FROM character_books
   INNER JOIN characters
-  ON characters.book_id = books.id"
+  ON characters.id = character_books.character_id"
 end
